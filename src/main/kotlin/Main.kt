07a2d72c4ec8569d756653ab28738ca565a04aaf
IvanovYurii <ivanovyura76@gmail.com>
2025-01-18 23:47:53 +0200

    Solution of this task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,15 @@
+const val ADULT_AGE = 18
+const val PERSON_AGE = 20
 
-// provide your solution below
+val Person.isAdult: Boolean
+    get() = age >= ADULT_AGE
+
+fun main() {
+    val person = Person("John Doe", PERSON_AGE)
+
+    if (person.isAdult) {
+        println("${person.name} is an adult.")
+    } else {
+        println("${person.name} is not an adult.")
+    }
+}
